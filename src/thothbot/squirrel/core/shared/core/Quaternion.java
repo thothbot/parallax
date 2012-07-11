59b2ff6e9d9d2f1e1990fbd40c3e98c59f734d68
thothbot <thothbot@gmail.com>
2012-07-11 19:07:51 +0400

    Added documentation for the Quaternion

--- src/thothbot/squirrel/core/shared/core/Quaternion.java
+++ src/thothbot/squirrel/core/shared/core/Quaternion.java
@@ -22,45 +22,61 @@
 
 package thothbot.squirrel.core.shared.core;
 
+/**
+ * Implementation of Quaternion which provide a convenient mathematical notation 
+ * for representing orientations and rotations of objects in three dimensions.
+ * 
+ * Quaternion represented by four coordinates: X, Y, Z, W
+ * 
+ * @author thothbot
+ *
+ */
 public class Quaternion
 {
 	/**
-	 * The x coordinate.
+	 * The X coordinate.
 	 */
-	public float x = 0.0f;
+	public float x;
 
 	/**
-	 * The y coordinate.
+	 * The Y coordinate.
 	 */
-	public float y = 0.0f;
+	public float y;
 
 	/**
-	 * The z coordinate.
+	 * The Z coordinate.
 	 */
-	public float z = 0.0f;
+	public float z;
 
 	/**
-	 * The w coordinate.
+	 * The W coordinate.
 	 */
-	public float w = 1;
+	public float w;
 
-	public Quaternion() {
+	/**
+	 * Default constructor will make Quaternion (0.0, 0.0, 0.0, 1.0)
+	 */
+	public Quaternion() 
+	{
+		this.x = 0.0f;
+		this.y = 0.0f;
+		this.z = 0.0f;
+		this.w = 1.0f;
 	}
 
 	/**
-	 * Constructs and initializes a Vector4f from the specified xyzw
+	 * Constructs and initializes a Quaternion from the specified X, Y, Z, W
 	 * coordinates.
 	 * 
-	 * @param x
-	 *            the x coordinate
-	 * @param y
-	 *            the y coordinate
-	 * @param z
-	 *            the z coordinate
-	 * @param w
-	 *            the w coordinate
+	 * Will make Quaternion (X, Y, Z, W)
+	 * 
+	 * @param x the X coordinate
+	 * @param y the Y coordinate
+	 * @param z the Z coordinate
+	 * @param w the W coordinate
 	 */
-	public Quaternion(float x, float y, float z, float w) {
+	public Quaternion(float x, float y, float z, float w) 
+	{
 		this.x = x;
 		this.y = y;
 		this.z = z;
@@ -68,21 +84,66 @@ public class Quaternion
 	}
 
 	/**
-	 * Constructs and initializes a Vector4f from the specified xyz coordinates.
+	 * Constructs and initializes a Quaternion from the specified X, Y, Z coordinates.
+	 * Will make Quaternion (X, Y, Z, 1.0)
 	 * 
-	 * @param x
-	 *            the x coordinate
-	 * @param y
-	 *            the y coordinate
-	 * @param z
-	 *            the z coordinate
+	 * @param x the x coordinate
+	 * @param y the y coordinate
+	 * @param z the z coordinate
 	 */
-	public Quaternion(float x, float y, float z) {
+	public Quaternion(float x, float y, float z) 
+	{
+		this();
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 
+	/**
+	 * Getting X coordinate from the Quaternion
+	 * 
+	 * @return a X coordinate
+	 */
+	public float getX()
+	{
+		return this.x;
+	}
+	
+	/**
+	 * Getting Y coordinate from the Quaternion
+	 * 
+	 * @return a Y coordinate
+	 */
+	public float getY()
+	{
+		return this.y;
+	}
+	
+	/**
+	 * Getting Z coordinate from the Quaternion
+	 * 
+	 * @return a Z coordinate
+	 */
+	public float getZ()
+	{
+		return this.z;
+	}
+	
+	/**
+	 * Getting W coordinate from the Quaternion
+	 * 
+	 * @return a W coordinate
+	 */
+	public float getW()
+	{
+		return this.w;
+	}
+	
+	/**
+	 * Copy values from input Quaternion to the values of current Quaternion.
+	 * 
+	 * @param c1 the input Quaternion
+	 */
 	public void copy(Quaternion c1)
 	{
 		this.x = c1.x;
@@ -157,6 +218,9 @@ public class Quaternion
 				* this.z));
 	}
 
+	/**
+	 * Negates the value of this Quaternion in place.
+	 */
 	public void inverse()
 	{
 		this.x *= -1;
@@ -170,19 +234,25 @@ public class Quaternion
 				* this.w);
 	}
 
+	/**
+	 * Normalize the current Quaternion
+	 */
 	public void normalize()
 	{
 		float l = (float) Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z + this.w
 				* this.w);
 
-		if (l == 0) {
+		if (l == 0) 
+		{
 
 			this.x = 0;
 			this.y = 0;
 			this.z = 0;
 			this.w = 0;
 
-		} else {
+		} 
+		else 
+		{
 
 			l = 1f / l;
 
@@ -194,21 +264,35 @@ public class Quaternion
 		}
 	}
 
+	/**
+	 * Sets the value of this Quaternion to the vector multiplication of Quaternion a and
+	 * Quaternion v2.
+	 * 
+	 * Based on:
+	 * {@linkplain http://www.euclideanspace.com/maths/algebra/realNormedAlgebra/quaternions/code/index.htm}
+	 * 
+	 * @param v1 the first Quaternion
+	 * @param v2 the second Quaternion
+	 * 
+	 */
 	public void multiply(Quaternion a, Quaternion b)
 	{
-
-		// from
-		// http://www.euclideanspace.com/maths/algebra/realNormedAlgebra/quaternions/code/index.htm
-
-		this.x = a.x * b.w + a.y * b.z - a.z * b.y + a.w * b.x;
+		this.x = a.x * b.w  + a.y * b.z - a.z * b.y + a.w * b.x;
 		this.y = -a.x * b.z + a.y * b.w + a.z * b.x + a.w * b.y;
-		this.z = a.x * b.y - a.y * b.x + a.z * b.w + a.w * b.z;
+		this.z = a.x * b.y  - a.y * b.x + a.z * b.w + a.w * b.z;
 		this.w = -a.x * b.x - a.y * b.y - a.z * b.z + a.w * b.w;
 	}
 
+	/**
+	 * Sets the value of this Quaternion to the vector multiplication of itself and
+	 * Quaternion b.
+	 * (this = this * b)
+	 * 
+	 * @param b the other Quaternion
+	 * 
+	 */
 	public void multiply(Quaternion b)
 	{
-
 		float qax = this.x, qay = this.y, qaz = this.z, qaw = this.w, qbx = b.x, qby = b.y, qbz = b.z, qbw = b.w;
 
 		this.x = qax * qbw + qaw * qbx + qay * qbz - qaz * qby;
@@ -217,15 +301,32 @@ public class Quaternion
 		this.w = qaw * qbw - qax * qbx - qay * qby - qaz * qbz;
 	}
 
+	/**
+	 * Sets the value of the input vector to the vector multiplication of input vector and
+	 * the current Quaternion.
+	 * 
+	 * @param vector the input vector
+	 * 
+	 * @return the modified input vector
+	 */
 	public void multiply(Vector3f vector)
 	{
 		multiply(vector, vector);
 	}
 
+	/**
+	 * Sets the value of the destination vector to the vector multiplication of vector and
+	 * the current Quaternion.
+	 * 
+	 * @param vector the input vector
+	 * @param dest   the destination vector
+	 * 
+	 * @return the modified destination vector
+	 */
 	public Vector3f multiply(Vector3f vector, Vector3f dest)
 	{
-
-		float x = vector.x, y = vector.y, z = vector.z, qx = this.x, qy = this.y, qz = this.z, qw = this.w;
+		float x = vector.getX(), y = vector.getY(), z = vector.getZ();
+		float qx = this.getX(), qy = this.getY(), qz = this.getZ(), qw = this.getW();
 
 		// calculate quat * vector
 		float ix = qw * x + qy * z - qz * y;
@@ -234,19 +335,28 @@ public class Quaternion
 		float iw = -qx * x - qy * y - qz * z;
 
 		// calculate result * inverse quat
-
-		dest.x = ix * qw + iw * -qx + iy * -qz - iz * -qy;
-		dest.y = iy * qw + iw * -qy + iz * -qx - ix * -qz;
-		dest.z = iz * qw + iw * -qz + ix * -qy - iy * -qx;
+		dest.setX( ix * qw + iw * -qx + iy * -qz - iz * -qy );
+		dest.setY( iy * qw + iw * -qy + iz * -qx - ix * -qz );
+		dest.setZ( iz * qw + iw * -qz + ix * -qy - iy * -qx );
 
 		return dest;
 	}
 
-	public Quaternion slerp(Quaternion qa, Quaternion qb, Quaternion qm, float t)
+	/**
+	 * Quaternion Interpolation
+	 * 
+	 * Based on:
+	 * {@linkplain http://www.euclideanspace.com/maths/algebra/realNormedAlgebra/quaternions/slerp/}
+	 * 
+	 * @param qa  the quaternion a (first quaternion to be interpolated between)
+	 * @param qb  the quaternion b (second quaternion to be interpolated between)
+	 * @param qm  the interpolated quaternion
+	 * @param t   a scalar between 0.0 (at qa) and 1.0 (at qb)
+	 * 
+	 * @return the interpolated quaternion
+	 */
+	public static Quaternion slerp(Quaternion qa, Quaternion qb, Quaternion qm, float t)
 	{
-
-		// http://www.euclideanspace.com/maths/algebra/realNormedAlgebra/quaternions/slerp/
-
 		float cosHalfTheta = qa.w * qb.w + qa.x * qb.x + qa.y * qb.y + qa.z * qb.z;
 
 		if (cosHalfTheta < 0) {
@@ -290,6 +400,12 @@ public class Quaternion
 		return qm;
 	}
 
+	/**
+	 * Clone the current Quaternion
+	 * quaternion.clone() != quaternion;
+	 * 
+	 * @return the new instance of Quaternion
+	 */
 	public Quaternion clone()
 	{
 		return new Quaternion(this.x, this.y, this.z, this.w);
