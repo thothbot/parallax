9b0eff819e4f875078476c8dde8f5e9609177aa0
thothbot <thothbot@gmail.com>
2012-07-24 15:27:42 +0400

    Updated DAE Skin parser

--- src/thothbot/parallax/loader/shared/collada/dae/DaeSkin.java
+++ src/thothbot/parallax/loader/shared/collada/dae/DaeSkin.java
@@ -19,8 +19,11 @@
 
 package thothbot.parallax.loader.shared.collada.dae;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
+import thothbot.parallax.core.shared.Log;
 import thothbot.parallax.core.shared.core.Matrix4f;
 
 import com.google.gwt.xml.client.Node;
@@ -35,13 +38,50 @@ public class DaeSkin extends DaeElement
 	{
 		super(node);
 
+		Log.debug("DaeSkin() " + toString());
 	}
 
 	@Override
 	public void read() 
 	{
+		sources = new HashMap<String, DaeSource>();
 
+		NodeList list = getNode().getChildNodes();
+		for (int i = 0; i < list.getLength(); i++) 
+		{
+			Node child = list.item(i);
+			String nodeName = child.getNodeName();
 
+			if (nodeName.compareTo("source") == 0) 
+			{
+				DaeSource source = new DaeSource(child);
+				sources.put(source.getID(), source);
+			} 
+			else if (nodeName.compareTo("bind_shape_matrix") == 0) 
+			{
+				DaeDummyElement bind_shape_matrix = new DaeDummyElement(child);
+				float[] data = bind_shape_matrix.readFloatArray();
+				bindShapeMatrix = new Matrix4f(
+						data[0], data[1], data[2], data[3],
+						data[4], data[5], data[6], data[7],
+						data[8], data[9], data[10], data[11],
+						data[12], data[13], data[14], data[15]
+					);
+			}
+			else if (nodeName.compareTo("joints") == 0) 
+			{
+//				primitives.add(new DaeTriangles(child, this));
+			}
+			else if (nodeName.compareTo("vertex_weights") == 0) 
+			{
+//				primitives.add(new DaeTriangles(child, this));
+			}
+		}
+	}
+	
+	public String toString()
+	{
+		return "bind_shape_matrix=" + this.bindShapeMatrix;
 	}
 
 }
