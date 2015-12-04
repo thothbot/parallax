bf3878c2a898851f76026efb358ee08acd77aa4e
thothbot <thothbot@gmail.com>
2015-12-04 21:30:18 +0300

    Updated project structure for the new builder

---
+++ core/src/parallax/core/client/controls/Controls.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2012 Alex Usachev, thothbot@gmail.com
+ * 
+ * This file is part of Parallax project.
+ * 
+ * Parallax is free software: you can redistribute it and/or modify it 
+ * under the terms of the Creative Commons Attribution 3.0 Unported License.
+ * 
+ * Parallax is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the Creative Commons Attribution 
+ * 3.0 Unported License. for more details.
+ * 
+ * You should have received a copy of the the Creative Commons Attribution 
+ * 3.0 Unported License along with Parallax. 
+ * If not, see http://creativecommons.org/licenses/by/3.0/.
+ */
+
+package src.parallax.core.client.controls;
+
+import org.parallax3d.parallax.core.client.context.Canvas3d;
+import org.parallax3d.parallax.core.shared.cameras.Camera;
+import org.parallax3d.parallax.core.shared.core.Object3D;
+
+import com.google.gwt.user.client.ui.Widget;
+import src.parallax.core.client.context.Canvas3d;
+import src.parallax.core.shared.cameras.Camera;
+import src.parallax.core.shared.core.Object3D;
+
+/**
+ * The abstract control implementation.
+ * 
+ * @author thothbot
+ *
+ */
+public abstract class Controls
+{
+	private Object3D object;
+	private Widget widget;
+	
+	/**
+	 * The constructor will create a {@link Controls} instance.
+	 * 
+	 * @param object the {@link Object3D} which will be controlled. 
+	 * 				For example {@link Camera} object.
+	 * @param widget the Widget where to listen control commands. 
+	 * 				Basically {@link Canvas3d} widget.
+	 */
+	public Controls(Object3D object, Widget widget)
+	{
+		this.object = object;
+		this.widget = widget;
+	}
+	
+	/**
+	 * Gets controlled instance.
+	 * 
+	 * @return the {@link Object3D}.
+	 */
+	public Object3D getObject()
+	{
+		return this.object;
+	}
+	
+	/**
+	 * Gets widget.
+	 * 
+	 * @return the Widget.
+	 */
+	public Widget getWidget()
+	{
+		return this.widget;
+	}
+}
---
+++ core/src/parallax/core/shared/objects/Bone.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2012 Alex Usachev, thothbot@gmail.com
+ * 
+ * This file is part of Parallax project.
+ * 
+ * Parallax is free software: you can redistribute it and/or modify it 
+ * under the terms of the Creative Commons Attribution 3.0 Unported License.
+ * 
+ * Parallax is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the Creative Commons Attribution 
+ * 3.0 Unported License. for more details.
+ * 
+ * You should have received a copy of the the Creative Commons Attribution 
+ * 3.0 Unported License along with Parallax. 
+ * If not, see http://creativecommons.org/licenses/by/3.0/.
+ */
+
+package src.parallax.core.shared.objects;
+
+import org.parallax3d.parallax.core.shared.core.Object3D;
+
+/**
+ * A bone which is part of a SkinnedMesh.
+ *
+ */
+public class Bone extends Object3D
+{
+
+	private SkinnedMesh skin;
+
+	/**
+	 * 
+	 * @param belongsToSkin An instance of {@link SkinnedMesh}.
+	 */
+	public Bone(SkinnedMesh belongsToSkin) 
+	{
+		this.setSkin(belongsToSkin);		
+	}
+
+	/**
+	 * @return the skin
+	 */
+	public SkinnedMesh getSkin() {
+		return skin;
+	}
+
+	/**
+	 * @param skin the skin to set
+	 */
+	public void setSkin(SkinnedMesh skin) {
+		this.skin = skin;
+	}
+
+}
