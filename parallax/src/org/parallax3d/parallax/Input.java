7985e13a9c6ec2f04f89ca3ec35cb67474ccbf29
thothbot <thothbot@gmail.com>
2016-01-30 23:05:18 +0300

    Added common Input interface

---
+++ parallax/src/org/parallax3d/parallax/Input.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2012 Alex Usachev, thothbot@gmail.com
+ *
+ * This file is part of Parallax project.
+ *
+ * Parallax is free software: you can redistribute it and/or modify it
+ * under the terms of the Creative Commons Attribution 3.0 Unported License.
+ *
+ * Parallax is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the Creative Commons Attribution
+ * 3.0 Unported License. for more details.
+ *
+ * You should have received a copy of the the Creative Commons Attribution
+ * 3.0 Unported License along with Parallax.
+ * If not, see http://creativecommons.org/licenses/by/3.0/.
+ */
+
+package org.parallax3d.parallax;
+
+public interface Input {
+
+    interface TextInputListener {
+        void input (String text);
+
+        void canceled ();
+    }
+
+    public static class Buttons {
+        public static final int LEFT = 0;
+        public static final int RIGHT = 1;
+        public static final int MIDDLE = 2;
+        public static final int BACK = 3;
+        public static final int FORWARD = 4;
+    }
+}
