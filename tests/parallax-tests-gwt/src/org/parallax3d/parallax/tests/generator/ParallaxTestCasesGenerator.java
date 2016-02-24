bb384a48d47d9fbe81af84d222e03c09d1fca0c8
thothbot <thothbot@gmail.com>
2016-02-24 21:01:22 +0300

    Refactored ParallaxTestCasesGenerator

--- tests/parallax-tests-gwt/src/org/parallax3d/parallax/tests/generator/ParallaxTestCasesGenerator.java
+++ tests/parallax-tests-gwt/src/org/parallax3d/parallax/tests/generator/ParallaxTestCasesGenerator.java
@@ -25,10 +25,14 @@ import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.core.ext.typeinfo.JClassType;
 import com.google.gwt.core.ext.typeinfo.JPackage;
 import com.google.gwt.core.ext.typeinfo.TypeOracle;
+import com.google.gwt.user.rebind.ClassSourceFileComposerFactory;
+import com.google.gwt.user.rebind.SourceWriter;
+import org.parallax3d.parallax.system.FastMap;
 import org.parallax3d.parallax.tests.ParallaxTest;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ParallaxTestCasesGenerator extends Generator {
@@ -36,73 +40,73 @@ public class ParallaxTestCasesGenerator extends Generator {
     @Override
     public String generate(TreeLogger logger, GeneratorContext context, String typeName) throws UnableToCompleteException {
 
-        PrintWriter pw = context.tryCreate(logger,
-                "org.parallax3d.parallax.tests",
-                "ParallaxTestCasesImpl");
-
-        if (pw != null)
-        {
-            // write package, imports, whatever
-            pw.append("package org.parallax3d.parallax.tests;");
-            pw.append("import org.parallax3d.parallax.system.FastMap;");
-            pw.append("import java.util.ArrayList;");
-            pw.append("import java.util.Arrays;");
-            pw.append("import java.util.List;");
-
-            // iterates over all the classes to find those with EntryPointWidget annotation
-            TypeOracle oracle = context.getTypeOracle();
-            JPackage[] packages = oracle.getPackages();
-
-            System.out.println(" Start GWT test generation ");
-
-            List<JClassType> classes = new ArrayList<>();
-            JClassType testClass = oracle.findType(ParallaxTest.class.getName());
-            for (JPackage pack : packages)
-                for (JClassType classtype : pack.getTypes())
-                    if(classtype != testClass && testClass.isAssignableFrom(classtype))
-                        classes.add(classtype);
-
-            System.out.println("   " + classes.size() + " test cases found");
-
-            // the class
-            pw.append("public class ParallaxTestCasesImpl implements ParallaxTestCases {");
-
-            pw.append("   static final FastMap<List<ParallaxTest>> byGroup = new FastMap<>();");
-            pw.append("   static final FastMap<ParallaxTest> byToken = new FastMap<>();");
-
-            pw.append("   static final List<ParallaxTest> all = new ArrayList<ParallaxTest>() {{");
-
-            for(JClassType cls: classes)
-                pw.append("      add( new " + cls.getQualifiedSourceName() + "() );");
-
-            pw.append("   }};");
-
-            pw.append("   static {");
-            pw.append("        for (ParallaxTest test : all) {");
-            pw.append("             byToken.put(test.getContentWidgetToken(), test);");
-            pw.append("             String group = getProposalGroupName(test.getClass().getName());");
-            pw.append("             if (byGroup.containsKey(group)) byGroup.get(group).add(test);");
-            pw.append("             else { List<ParallaxTest> tests = new ArrayList<>(); tests.add(test); byGroup.put(group, tests);}");
-            pw.append("        }");
-            pw.append("   }");
-
-            // get method
-            pw.append("   private static String getProposalGroupName(String cls) {");
-            pw.append("       String name = cls.substring(0, cls.lastIndexOf(\".\") ).replace(\"org.parallax3d.parallax.tests.cases\",\"\");");
-            pw.append("       if(name.length() == 0) name = \"Unspecified\";");
-            pw.append("       else name = name.substring(1);");
-            pw.append("       return Character.toString(name.charAt(0)).toUpperCase() + name.substring(1);");
-            pw.append("   }"); // method
-            pw.append("   public FastMap<List<ParallaxTest>> getAllTests() { return byGroup; }"); // method
-            pw.append("   public ParallaxTest getContentWidgetForToken(String token) { return byToken.get(token); }"); // method
-            pw.append("}"); // class
-
-            context.commit(logger, pw);
+        TypeOracle oracle = context.getTypeOracle();
+        JClassType toGenerate = oracle.findType(typeName).isInterface();
+        if (toGenerate == null) {
+            logger.log(TreeLogger.ERROR, typeName + " is not an interface type");
+            throw new UnableToCompleteException();
         }
 
-        // return the name of the generated class
-        return "org.parallax3d.parallax.tests.ParallaxTestCasesImpl";
-    }
+        String packageName = toGenerate.getPackage().getName();
+        String simpleSourceName = toGenerate.getName().replace('.', '_') + "Impl";
+        PrintWriter pw = context.tryCreate(logger, packageName, simpleSourceName);
+        if (pw == null) {
+            return packageName + "." + simpleSourceName;
+        }
 
+        ClassSourceFileComposerFactory factory = new ClassSourceFileComposerFactory(packageName, simpleSourceName);
+
+        factory.addImplementedInterface(typeName);
+        factory.addImport(ArrayList.class.getName());
+        factory.addImport(Arrays.class.getName());
+        factory.addImport(List.class.getName());
+        factory.addImport(FastMap.class.getName());
+
+        System.out.println(" Start GWT test generation ");
+
+        
+        List<JClassType> classes = new ArrayList<>();
+        JClassType testClass = oracle.findType(ParallaxTest.class.getName());
+        for (JPackage pack : oracle.getPackages())
+            for (JClassType classtype : pack.getTypes())
+                if(classtype != testClass && testClass.isAssignableFrom(classtype))
+                    classes.add(classtype);
+
+        System.out.println("   " + classes.size() + " test cases found");
+
+        SourceWriter sw = factory.createSourceWriter(context, pw);
+        
+        sw.println("static final FastMap<List<ParallaxTest>> byGroup = new FastMap<>();");
+        sw.println("static final FastMap<ParallaxTest> byToken = new FastMap<>();");
+
+        sw.println("static final List<ParallaxTest> all = new ArrayList<ParallaxTest>() {{");
+
+        for(JClassType cls: classes)
+            sw.println("add( new " + cls.getQualifiedSourceName() + "() );");
+
+        sw.println("}};");
+
+        sw.println("static {");
+            sw.println("for (ParallaxTest test : all) {");
+                sw.println("byToken.put(test.getContentWidgetToken(), test);");
+                sw.println("String group = getProposalGroupName(test.getClass().getName());");
+                sw.println("if (byGroup.containsKey(group)) byGroup.get(group).add(test);");
+                sw.println("else { List<ParallaxTest> tests = new ArrayList<>(); tests.add(test); byGroup.put(group, tests);}");
+            sw.println("}");
+        sw.println("}");
+
+        // get method
+        sw.println("private static String getProposalGroupName(String cls) {");
+            sw.println("String name = cls.substring(0, cls.lastIndexOf(\".\") ).replace(\"org.parallax3d.parallax.tests.cases\",\"\");");
+            sw.println("if(name.length() == 0) name = \"Unspecified\";");
+            sw.println("else name = name.substring(1);");
+            sw.println("return Character.toString(name.charAt(0)).toUpperCase() + name.substring(1);");
+        sw.println("}"); // method
+        sw.println("public FastMap<List<ParallaxTest>> getAllTests() { return byGroup; }"); // method
+        sw.println("public ParallaxTest getContentWidgetForToken(String token) { return byToken.get(token); }"); // method
+
+        sw.commit(logger);
+        return factory.getCreatedClassName();
+    }
 
 }
