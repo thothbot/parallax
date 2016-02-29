f10281e7aac4e68ea06d2d4bea09cc3bd0cf223a
thothbot <thothbot@gmail.com>
2016-02-29 21:15:06 +0300

    Fixed gwt regexp

--- platforms/parallax-gwt/src/org/parallax3d/parallax/platforms/gwt/jre/java/util/regex/Pattern.java
+++ platforms/parallax-gwt/src/org/parallax3d/parallax/platforms/gwt/jre/java/util/regex/Pattern.java
@@ -1,18 +1,21 @@
-/*******************************************************************************
- * Copyright 2011 See AUTHORS file.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
+/*
+ * Copyright 2012 Alex Usachev, thothbot@gmail.com
+ * This file is based on libgdx code
+ *
+ * This file is part of Parallax project.
+ *
+ * Parallax is free software: you can redistribute it and/or modify it
+ * under the terms of the Creative Commons Attribution 3.0 Unported License.
+ *
+ * Parallax is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the Creative Commons Attribution
+ * 3.0 Unported License. for more details.
+ *
+ * You should have received a copy of the the Creative Commons Attribution
+ * 3.0 Unported License along with Parallax.
+ * If not, see http://creativecommons.org/licenses/by/3.0/.
+ */
 
 package java.util.regex;
 
@@ -24,7 +27,7 @@ public class Pattern {
 	final RegExp regExp;
 
 	private Pattern (String regExp) {
-		this.regExp = RegExp.compile(regExp);
+		this.regExp = RegExp.compile(regExp, "g");
 	}
 
 	public static Pattern compile (String regExp) {
