454d39fa2ee2b0243fdba279275247b9e88ef7b3
samarhxc <samarhxc@gmail.com>
2016-03-16 15:59:56 +0100

    Fix pressed buttons list.

--- platforms/parallax-gwt/src/org/parallax3d/parallax/platforms/gwt/GwtInput.java
+++ platforms/parallax-gwt/src/org/parallax3d/parallax/platforms/gwt/GwtInput.java
@@ -19,6 +19,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.parallax3d.parallax.Input;
 import org.parallax3d.parallax.input.InputHandler;
